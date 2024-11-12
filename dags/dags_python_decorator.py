32485577392863144f54ee9ff4068ab934752bfe
goodlucktokdy <kd01051@gmail.com>
2024-11-12 22:27:15 +0900

    python_decorator 실습

--- dags/dags_python_decorator.py
+++ dags/dags_python_decorator.py
@@ -7,11 +7,12 @@ with DAG(
     dag_id = 'dags_python_decorator',
     schedule = '30 22 * * 2',
     start_date = pendulum.datetime(2024,11,12,tz='Asia/Seoul'),
-    tags = ['practice']
+    tags = ['practice'],
+    catchup = False,
 ) as dag:
     
     @task(task_id = 'python_task_1')
     def print_context(some_input):
         print(some_input)
-    
+
     python_task_1 = print_context('task_decorator 실행')
