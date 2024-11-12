8972bee1ee450fcc5b4165560ac51d19fb875c82
goodlucktokdy <kd01051@gmail.com>
2024-11-12 22:39:13 +0900

    python_args_kwargs 실습

---
+++ dags/dags_python_args_kwargs.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from common.common_func import regist2
+import pendulum
+import datetime 
+
+with DAG(
+    dag_id = 'dags_python_args_kwargs',
+    schedule = '45 22 * * *',
+    start_date = pendulum.datetime(2024,11,12,tz='Asia/Seoul'),
+    tags = ['practice'],
+    catchup = False
+) as dag:
+    
+    python_t1 = PythonOperator(
+        task_id = 'python_t1',
+        python_callable = regist2,
+        op_args = ['Kim.D.Y','Male','Goyang City','Ilsan']
+        op_kwars = {email : 'kd01051@naver.com',phone : '010-2713-5749'}
+    )
+
+    python_t1
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,2 +1,14 @@
 def get_sftp():
     print('sftp 작업을 시작합니다.')
+def regist2(name, sex, *args, **kwars):
+    print(name)
+    print(sex)
+    print(*args)
+    email = kwargs['email'] or None
+    phone = kwargs['phone'] or None
+
+    if email: 
+        print(f'email 주소: {email}')
+    if phone: 
+        print(f'phone 번호: {phone}')
+
