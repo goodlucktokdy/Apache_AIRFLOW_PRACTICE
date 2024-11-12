1c65c2f315d51a790233dc7726c2dac570836e5e
goodlucktokdy <kd01051@gmail.com>
2024-11-12 22:24:51 +0900

    python_decorator_실습

---
+++ dags/dags_python_decorator.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.decorators import task 
+import pendulum
+import datetime  
+
+with DAG(
+    dag_id = 'dags_python_decorator',
+    schedule = '30 22 * * 2',
+    start_date = pendulum.datetime(2024,11,12,tz='Asia/Seoul'),
+    tags = ['practice']
+) as dag:
+    
+    @task(task_id = 'python_task_1')
+    def print_context(some_input):
+        print(some_input)
+    
+    python_task_1 = print_context('task_decorator 실행')
