1825a61a502a791393acae9d9fa472456709c296
goodlucktokdy <kd01051@gmail.com>
2024-11-12 22:00:47 +0900

    python 외부함수 실습

---
+++ dags/dags_python_import_func.py
@@ -0,0 +1,18 @@
+from airflow import DAG 
+from airflow.operators.python import PythonOperator
+from common.common_func import get_sftp
+import datetime
+import pendulum
+
+with DAG(
+    dag_id = 'dags_python_import_func',
+    schedule = '0 22 * * 3#2',
+    catchup = False,
+    tag = 'practice'
+) as dag:
+    python_t1 = PythonOperator(
+        task_id = 'python_t1',
+        python_callable = get_sftp
+    )
+
+    python_t1
