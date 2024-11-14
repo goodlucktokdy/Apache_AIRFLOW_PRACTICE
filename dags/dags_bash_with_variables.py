10fc2c3eb2c1e97a74420f7680a3142fc31fe666
goodlucktokdy <kd01051@gmail.com>
2024-11-14 15:15:49 +0900

    dags_bash_with_variables

---
+++ dags/dags_bash_with_variables.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.modles import Variable
+import datetime
+import pendulum
+
+with DAG(
+    dag_id = 'dags_bash_with_variables',
+    schedule = '15 9 * * 1-5',
+    start_date = pendulum.datetime(2024,11,10,tz='Asia/Seoul'),
+    tags = ['practice','bash','variables'],
+    catchup = False
+) as dag:
+    
+    variable = Variable.get('sample_key')
+
+    bash_t1 = BashOperator(
+        task_id = 'bash_t1',
+        bash_command = 'echo "전역변수 Variable.get("key"): {variable}'
+    )
+
+    bash_t2 = BashOperator(
+        task_id = 'bash_t2',
+        bash_command = 'echo "{{var.value.sample_key)}}"'
+    )
+
+    bash_t1 >> bash_t2
