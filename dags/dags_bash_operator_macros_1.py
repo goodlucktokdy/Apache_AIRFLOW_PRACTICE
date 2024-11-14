fdfc25769f584e68699470a252b50a8e6103046c
goodlucktokdy <kd01051@gmail.com>
2024-11-14 11:21:45 +0900

    dags_bash_operator_macros_relativedelta

--- dags/dags_bash_operator_macros_1.py
+++ dags/dags_bash_operator_macros_1.py
@@ -14,8 +14,8 @@ with DAG(
     # --start_date: 전월말일, --end_date: 현재 배치 1일전
     bash_t1 = BashOperator(
         task_id = 'bash_t1',
-        env = {'start_date':'{{data_interval_start.in_timezone('Asia/Seoul')| ds}}',
-               'end_date':'{{(data_interval_end.in_timezone('Asia/Seoul') - macros.relativedelta.relativedelta(days=1)) | ds}}'},
+        env = {'start_date':'{{data_interval_start.in_timezone("Asia/Seoul")| ds}}',
+               'end_date':'{{(data_interval_end.in_timezone("Asia/Seoul") - macros.relativedelta.relativedelta(days=1)) | ds}}'},
         bash_command = 'echo "start_date: $start_date" && echo "end_date: $end_date"'
     )
 
